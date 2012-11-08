38fc17aa37fd7ff744bdf19d2f222edf488711d0
Chris Carter <chris.carter1@ntlworld.com>
2012-11-08 13:21:16 +0000

    Generated a new Rails project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DoctorPrototype::Application.config.secret_token = 'bccc291d1aa85e39bf3d30aec88854aa1bc24e6ef2e0861c064c352f01690225a57b26553029aa88b4e71591630927a1bc8c25480cb7c8d18dc71f7eb366c878'
