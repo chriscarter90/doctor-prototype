36b6e6d4eec7f92115c847d70391b3c8790a0156
Chris Carter <chris.carter1@ntlworld.com>
2012-11-23 19:07:58 +0000

    Allow dots in the id for a LectureCourse.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ DoctorPrototype::Application.routes.draw do
 
   root :to => 'home#index'
 
-  resources :lecture_courses, :only => [:index, :show]
+  resources :lecture_courses, :only => [:index, :show], :constraints => { :id => /[^\/]+(?=\.html\z|\.json\z)|[^\/]+/ }
   resources :degree_classes, :only => [:index, :show]
   resources :staff_members, :only => [:index, :show]
 
