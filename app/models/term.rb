bd4852a6a8eba4db582dcef32240261b03486118
Chris Carter <chris.carter1@ntlworld.com>
2012-12-10 16:53:22 +0000

    Ensure that a Term must have at least one week.

--- app/models/term.rb
+++ app/models/term.rb
@@ -4,8 +4,14 @@ class Term < ActiveRecord::Base
 
   # = Validations =
   validates_presence_of :no, :year
+  validate :must_have_at_least_one_week
 
   # = Relationships =
   belongs_to :year
   has_many :weeks
+
+  private
+  def must_have_at_least_one_week
+    errors.add(:weeks, :too_short, :count => 1) if weeks.blank?
+  end
 end
--- spec/factories/terms.rb
+++ spec/factories/terms.rb
@@ -4,5 +4,9 @@ FactoryGirl.define do
   factory :term do
     year
     no        { 1 + rand(3) }
+
+    after(:build) do |term|
+      term.weeks.push(FactoryGirl.create(:week, :term => term))
+    end
   end
 end
