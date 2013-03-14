8bc1a385ae40389449d910d985c086f1d747f15a
Chris Carter <chris.carter1@ntlworld.com>
2013-03-14 10:54:16 +0000

    Extract back buttons into helper method.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def back_button(text, link)
+    button_text = content_tag("i", "", :class => 'icon-chevron-left') + " Back to #{text}"
+    back = link_to(button_text, link, :class => "btn btn-danger")
+  end
 end
