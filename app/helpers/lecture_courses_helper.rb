79a92dc2112055503842f3054dfc5ae73c8c33ca
Chris Carter <chris.carter1@ntlworld.com>
2013-06-02 20:57:29 +0100

    Add a bit more description to the types on the UI.

--- app/helpers/lecture_courses_helper.rb
+++ app/helpers/lecture_courses_helper.rb
@@ -14,9 +14,22 @@ module LectureCoursesHelper
 
   def scheduling_type_options(course)
     options = []
-    types = ('a'..'c').to_a + ('b1' .. 'b4').to_a + ('c1' .. 'c4').to_a
+    types = [
+      ['a', ''],
+      ['b', ''],
+      ['c', ''],
+      ['b1', ' (LT-L)'],
+      ['b2', ' (L-LT)'],
+      ['b3', ' (L-TL)'],
+      ['b4', ' (TL-L)'],
+      ['c1', ' (LT-L)'],
+      ['c2', ' (L-LT)'],
+      ['c3', ' (L-TL)'],
+      ['c4', ' (TL-L)']
+    ]
+
     types.each do |type|
-      options << ["Type #{type}", type]
+      options << ["Type #{type[0]}#{type[1]}", type[0]]
     end
 
     options_for_select(options, course.scheduling_type)
